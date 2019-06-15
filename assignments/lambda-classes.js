fcf38f7484224f39d95636e2e42a4a3d4c0ab338
3Dia <tmgantt85@gmail.com>
2019-06-15 14:36:20 -0400

    save

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,27 @@
 // CODE here for your Lambda Classes
+
+class Person{
+    constructor(props){
+        this.personName = props.name;
+        this.personAge =props.age;
+        this.personLocation =props.location;
+    }
+    speak(){
+        return `Hello my name is ${this.personName}, I am from ${this.personLocation}`
+    }
+}
+
+const fred = new Person({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37
+})
+
+console.log(fred);
+console.log(fred.speak());
+
+class Instructor extends Person{
+    constructor(instructorProps){
+
+    }
+}
